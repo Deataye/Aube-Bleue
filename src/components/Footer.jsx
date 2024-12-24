993db0ce8b42170e6b1455a76158d222cc0e3171
Deataye <shahzaiba61@gmail.com>
2024-12-24 17:32:35 +0500

    checking again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-
+import React from 'react';
 
 const Footer = () => {
   return (
-    <section className='bg-[#C7C3E0] w-full h-[182px] flex flex-row  '>
-    <div className="    px-6 py-10 lg:py-16 lg:w-[1200px]   ">
+    <section className='bg-[#C7C3E0] w-full h-[182px] flex justify-between items-center px-6'>
+      <div className="flex items-center justify-start px-6 py-10 lg:py-16">
         <p>Aube Bleue © 2024 – Tous droits réservés.</p>
-        
-    </div>
-    <div className=" px-6 py-4 lg:py-6    overflow-hidden">
+      </div>
+      <div className="flex items-center justify-end px-6 py-4 lg:py-6">
         {/* Logo */}
-        <div className="">
+        <div>
           <img
             src="../src/assets/logo.svg" // Replace with your logo path
             alt="Aube Bleue Logo"
@@ -19,8 +17,7 @@ const Footer = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-
-export default Footer
+export default Footer;
